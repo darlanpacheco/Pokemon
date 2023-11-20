e168fd94c677d7124f03d291f288901fb69142e3
Darlan Pacheco <darlanpacheco@proton.me>
2023-11-20 16:26:24 -0300

    First step.

---
+++ scripts/pokemons.ts
@@ -0,0 +1,12 @@
+class Pokemon {
+  _name: string;
+  _type: string;
+  _health: number;
+  _attack: number;
+  constructor(name: string, type: string, health: number, attack: number) {
+    this._name = name;
+    this._type = type;
+    this._health = health;
+    this._attack = attack;
+  }
+}
