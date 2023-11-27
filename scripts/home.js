8a0181e13f27303dcd62c8346e81c137663ad048
Darlan Pacheco <darlanpacheco@proton.me>
2023-11-26 21:32:14 -0300

    Pokemon names in Home page.

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,14 @@
+const storagePokemon = localStorage.getItem("Pokemon");
+
 function pokemonImg() {
   document.querySelector("#profile>img").src =
-    "../images/" + localStorage.getItem("Pokemon") + ".png";
+    "../images/" + storagePokemon + ".png";
+}
+
+function pokemonName() {
+  document.querySelector("#name").innerHTML =
+    storagePokemon.charAt(0).toUpperCase() + storagePokemon.slice(1);
 }
 
 pokemonImg();
+pokemonName();
