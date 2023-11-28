8535175c079bd81bb736dbc6b4c4c0c663f3bea4
Darlan Pacheco <darlanpacheco@proton.me>
2023-11-28 15:55:32 -0300

    Home script, style and html update.

--- scripts/home.js
+++ scripts/home.js
@@ -3,11 +3,20 @@ const storagePokemon = localStorage.getItem("Pokemon");
 function pokemonImg() {
   document.querySelector("#profile>img").src =
     "../images/" + storagePokemon + ".png";
+  document.querySelector("#profile>img").alt = storagePokemon;
 }
 
 function pokemonName() {
   document.querySelector("#name").innerHTML =
     storagePokemon.charAt(0).toUpperCase() + storagePokemon.slice(1);
+}
+
+function circleCreator() {
+  let circle = document.createElement("div");
+  circle.style.width = "10px";
+  circle.style.height = "10px";
+  circle.style.backgroundColor = "white";
+  circle.style.borderRadius = "50%";
 }
 
 pokemonImg();
