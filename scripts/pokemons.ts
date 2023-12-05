4064719444e8b9b75f6f40892d48ebbb67e68c99
Darlan Pacheco <darlanpacheco@proton.me>
2023-12-05 20:54:03 -0300

    Project remake.

--- scripts/pokemons.ts
+++ scripts/pokemons.ts
@@ -3,13 +3,20 @@ class Pokemon {
   _type: string;
   _health: number;
   _attack: number;
-  constructor(name: string, type: string, health: number, attack: number) {
+  constructor(name: string, type: string, attack: number, health: number) {
     this._name = name;
     this._type = type;
-    this._health = health;
     this._attack = attack;
+    this._health = health;
   }
 }
 
 const type: string[] = ["Grass", "Water", "Fire", "Electric"];
-const pokemons: Pokemon[] = [new Pokemon("Pikachu", "Electric", 100, 100)];
+
+const pokemons: Pokemon[] = [
+  new Pokemon("Pikachu", "Electric", 125, 125),
+  new Pokemon("Charmander", "Fire", 150, 100),
+  new Pokemon("Buizel", "Water", 125, 125),
+  new Pokemon("Bulbasaur", "Grass", 100, 150),
+  new Pokemon("Squirtle", "Water", 100, 150),
+];
